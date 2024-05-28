41315c70e92c6f2edb1c92383c04b46efb1d02d8
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-28 15:59:58 +0300

    Update cashier.java

--- cashier.java
+++ cashier.java
@@ -1,1 +1,46 @@
 
+import java.time.LocalDateTime;
+
+
+public class cashier extends Employee {
+
+
+
+    private LocalDateTime checkInTime;
+    private LocalDateTime checkOutTime;
+
+    public cashier(  String name, int age, int ID, String password,int salary) {
+
+        super(  name,  age,  ID,  password , salary);
+    }
+
+    @Override
+    public String display_info() {
+        return "cashier{" +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", ID=" + ID +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public  void CheckIn(){
+
+        checkInTime = LocalDateTime.now();
+        System.out.println("Cashier checked in at: " + checkInTime);
+
+
+    }
+    @Override
+   public  void CheckOut(){
+
+
+    checkOutTime = LocalDateTime.now();
+    System.out.println("Cashier checked out at" +checkOutTime);
+
+
+    }
+
+}
+
