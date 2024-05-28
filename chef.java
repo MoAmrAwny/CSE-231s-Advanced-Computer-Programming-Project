76c4542c552a08402cc283b9fc9543064f66ebcb
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-28 16:01:07 +0300

    Update chef.java

--- chef.java
+++ chef.java
@@ -1,1 +1,49 @@
 
+import java.time.LocalDateTime;
+
+public class chef extends Employee{
+
+
+
+    String password;
+
+    private LocalDateTime checkInTime;
+    private LocalDateTime checkOutTime;
+
+    public chef(  String name, int age, int ID, String password,int salary) {
+
+       super( name,  age,  ID,  password, salary);
+    }
+
+    @Override
+    public String display_info() {
+        return "chef{" +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", ID=" + ID +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+     public void CheckIn(){
+
+        checkInTime = LocalDateTime.now();
+        System.out.println("Chef checked in at: " + checkInTime);
+
+
+    }
+    @Override
+    public  void CheckOut(){
+
+
+        checkOutTime = LocalDateTime.now();
+        System.out.println("Cashier checked out at" +checkOutTime);
+
+
+
+    }
+
+
+}
+
