b92dfa3929ec76a0d793d89cdf619ebd0977c67e
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-28 16:02:19 +0300

    Update DessertItems.java

--- DessertItems.java
+++ DessertItems.java
@@ -1,1 +1,28 @@
+public class DessertItems implements Items,Comparable<DessertItems> {
 
+    private String name;
+    private double price;
+    private String type;
+
+    public DessertItems(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    @Override
+    public void DisplayDetails() {
+        System.out.println("Name: " + name + ", Price: $" + price + ", Type: " + type);
+    }
+
+    @Override
+    public int compareTo(DessertItems o) {
+        return Double.compare(this.price, o.price);
+    }
+    public String getname() {
+        return name;
+    }
+
+    @Override
+    public double getprice() {
+        return price;
+    }
+}
