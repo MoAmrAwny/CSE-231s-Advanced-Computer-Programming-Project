0b4121aa6e3fbcfa1a5caa8891b650dfa57dee84
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-28 16:03:50 +0300

    Update DrinkItems.java

--- DrinkItems.java
+++ DrinkItems.java
@@ -1,1 +1,35 @@
 
+public class DrinkItems implements Items,Comparable<DrinkItems> {
+
+
+    private String name;
+    private double price;
+    private String size;
+
+    public DrinkItems(String name, double price) {
+        this.name = name;
+        this.price = price;
+
+    }
+
+
+    @Override
+    public int compareTo(DrinkItems other) {
+        // Compare drink items based on their prices
+        return Double.compare(this.price, other.price);
+    }
+
+    @Override
+    public void DisplayDetails() {
+        System.out.println("Name: " + name + ", Price: $" + price + ", Size: " + size);
+    }
+    public String getname() {
+        return name;
+    }
+
+    @Override
+    public double getprice() {
+        return price;
+    }
+}
+
