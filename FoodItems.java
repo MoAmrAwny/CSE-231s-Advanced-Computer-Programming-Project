56fd150cedec3670fd951eeca8611d9ffb66bfdf
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-28 16:06:10 +0300

    Update FoodItems.java

--- FoodItems.java
+++ FoodItems.java
@@ -1,1 +1,40 @@
 
+class FoodItems implements Items, Comparable<FoodItems> {
+    private String name;
+    private double price;
+
+
+    public FoodItems(String name, double price ){
+        this.name = name;
+        this.price = price;
+
+    }
+
+
+
+    @Override
+    public int compareTo(FoodItems other) {
+        // Compare food items based on their names
+        return Double.compare(this.price, other.price);
+    }
+
+
+    @Override
+    public void DisplayDetails() {
+        System.out.println("Name: " + name + ", Price: $" + price );
+    }
+
+    @Override
+    public String getname() {
+        return name;
+    }
+
+    @Override
+    public double getprice() {
+        return price;
+    }
+
+
+
+
+}
